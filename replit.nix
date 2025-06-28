6e5077c5ca73d0802d83a9d5aeca878ec157409e
rfauzannn <rizkcofauzana@gmail.com>
2025-06-28 15:53:00 +0700

    new

--- replit.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }: {
-  deps = [
-    pkgs.python310Full
-    pkgs.python310Packages.pip
-    pkgs.python310Packages.setuptools
-    pkgs.python310Packages.wheel
-    pkgs.python310Packages.numpy
-  ];
-}
